297517695c7fdec27f7a3c35b543a3c3c6d1f07b
AdityaLadage <46342797+AdityaLadage@users.noreply.github.com>
2019-01-15 23:10:14 +0530

    Update Solitaire.java

--- Solitaire.java
+++ Solitaire.java
@@ -5,27 +5,14 @@ import java.io.InputStreamReader;
 
 public class Solitaire {
 
-    private Stack cardStack1;
-    private Stack cardStack2;
-    private Stack cardStack3;
-    private Stack cardStack4;
-    private Stack cardStack5;
-    private Stack cardStack6;
-    private Stack cardStack7;
-    private Stack endStack1;
-    private Stack endStack2;
-    private Stack endStack3;
-    private Stack endStack4;
     private Stack tempStack;
     private CircularList pack;
+    private Stack[] cardStacks = new Stack[7];
+    private Stack[] endStacks = new Stack[4];
 
     public static void main(String[] args) throws IOException {
         Solitaire game = new Solitaire();
-        /*while(!game.isGameComplete()) {
-            if(!game.checkEndStack())
-                if(!game.checkCardStack())
-                    game.checkPack();
-        }*/
+        game.displayGame();
 
         System.out.println("*************** Choose Your Move ***************");
         System.out.println("1. Open Pack");
@@ -35,9 +22,9 @@ public class Solitaire {
         System.out.println("5. CardStack to CardStack");
         System.out.println("6. Open CardStack");
         System.out.println("7. Shift King to empty CardStack");
-        System.out.println("8. PutBack Pack\n");
+        System.out.println("8. PutBack Pack");
         System.out.println("9. Move Multiple Cards from CardStack to CardStack\n");
-        System.out.println("Enter your move: ");
+        System.out.print("Enter your move: ");
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -51,7 +38,7 @@ public class Solitaire {
                     game.pack.open();
                     break;
                 case 2:
-                    System.out.print("Enter the End Stack(Hearts/Spades/Diamonds/Clubs): ");
+                    System.out.print("Enter the End Stack(H/S/D/C): ");
                     System.out.println(game.movePackToEndStack(br.readLine()));
                     break;
                 case 3:
@@ -61,7 +48,7 @@ public class Solitaire {
                 case 4:
                     System.out.print("Enter the Card Stack(1/2/3/4/5/6/7): ");
                     name1 = Integer.parseInt(br.readLine());
-                    System.out.print("Enter the End Stack(Hearts/Spades/Diamonds/Clubs): ");
+                    System.out.print("Enter the End Stack(H/S/D/C): ");
                     name3 = br.readLine();
                     System.out.println(game.moveCardStackToEndStack(name1, name3));
                     break;
@@ -93,6 +80,7 @@ public class Solitaire {
                     name2 = Integer.parseInt(br.readLine());
                     System.out.println(game.moveMultipleCardStackToCardStack(name1, name2));
             }
+            game.displayGame();
             System.out.println("Enter your next move: ");
             val = Integer.parseInt(br.readLine());
         }
@@ -100,28 +88,35 @@ public class Solitaire {
     }
 
     private boolean isGameComplete() {
-        return endStack1.getTop() == 12 && endStack2.getTop() == 12 && endStack3.getTop() == 12 && endStack4.getTop() == 12;
+        for (int i = 0; i < 4; i++) {
+            if(endStacks[i].getTop() < 12)
+                return false;
+        }
+        return true;
     }
 
     private String movePackToEndStack(String name) {
-        if (name.equals(pack.open().getSuit())) {
-            if (name.equals("Hearts") && pack.open().getFaceName() == endStack1.peek().getFaceName() + 1) {
-                endStack1.push(pack.pop());
-            } else if (name.equals("Spades") && pack.open().getFaceName() == endStack2.peek().getFaceName() + 1) {
-                endStack2.push(pack.pop());
-            } else if (name.equals("Diamonds") && pack.open().getFaceName() == endStack3.peek().getFaceName() + 1) {
-                endStack3.push(pack.pop());
-            } else if (name.equals("Clubs") && pack.open().getFaceName() == endStack4.peek().getFaceName() + 1) {
-                endStack4.push(pack.pop());
-            } else {
-                return "Invalid Move.";
-            }
+        Stack move2 = stringToEndStack(name);
+        if (move2.isEmpty()) {
+            move2.push(pack.pop());
+            return "Move Completed.";
         }
-        return "Move Completed.";
+        if (move2.peek().getSuit().equals(pack.open().getSuit()))
+            if (pack.open().getFaceName() == move2.peek().getFaceName() + 1) {
+                move2.push(pack.pop());
+                return "Move Completed.";
+            }
+        return "Invalid Move.";
     }
 
     private String movePackToCardStack(int name) {
-        Stack move = integerToCardStack(name);
+        Stack move = cardStacks[name-1];
+        if(move.isEmpty() && pack.open().getFaceName() == 13) {
+            move.push(pack.pop());
+            move.setMaxOpen(move.getMaxOpen()+1);
+            move.setMinOpen(move.getMinOpen()+1);
+            return "Move Completed";
+        }
         if (!move.peek().getColor().equals(pack.open().getColor())) {
             if (move.peek().getFaceName() == pack.open().getFaceName() + 1) {
                 move.push(pack.pop());
@@ -133,8 +128,13 @@ public class Solitaire {
     }
 
     private String moveCardStackToEndStack(int name1, String name2) {
-        Stack move1 = integerToCardStack(name1);
+        Stack move1 = cardStacks[name1-1];
         Stack move2 = stringToEndStack(name2);
+        if (move2.isEmpty()) {
+            move2.push(move1.pop());
+            move1.setMaxOpen(move1.getMaxOpen() - 1);
+            return "Move Completed.";
+        }
         if (move2.peek().getSuit().equals(move1.peek().getSuit()))
             if (move1.peek().getFaceName() == move2.peek().getFaceName() + 1) {
                 move2.push(move1.pop());
@@ -145,11 +145,11 @@ public class Solitaire {
     }
 
     private String moveCardStackToCardStack(int name1, int name2) {
-        Stack move1 = integerToCardStack(name1);
-        Stack move2 = integerToCardStack(name2);
+        Stack move1 = cardStacks[name1-1];
+        Stack move2 = cardStacks[name2-1];
         if (!move1.peek().getColor().equals(move2.peek().getColor()))
-            if (move1.peek().getFaceName() == move2.peek().getFaceName() + 1) {
-                move1.push(move2.pop());
+            if (move1.peek().getFaceName() == move2.peek().getFaceName() - 1) {
+                move2.push(move1.pop());
                 move1.setMaxOpen(move1.getMaxOpen() - 1);
                 move2.setMaxOpen(move2.getMaxOpen() + 1);
                 return "Move Completed.";
@@ -158,9 +158,8 @@ public class Solitaire {
     }
 
     private String openCardStack(int name) {
-        Stack move = integerToCardStack(name);
+        Stack move = cardStacks[name-1];
         if (move.getMinOpen() > move.getMaxOpen() && move.getMinOpen() == move.getTop()+1) {
-            move.setMaxOpen(move.getMaxOpen()-1);
             move.setMinOpen(move.getMinOpen()-1);
             return "Move Completed.";
         }
@@ -168,8 +167,8 @@ public class Solitaire {
     }
 
     private String moveKingToCardStack(int name1, int name2) {
-        Stack move1 = integerToCardStack(name1);
-        Stack move2 = integerToCardStack(name2);
+        Stack move1 = cardStacks[name1-1];
+        Stack move2 = cardStacks[name2-1];
         if (move2.isEmpty()) {
             for (int i = move1.getMaxOpen(); i >= move1.getMinOpen(); i--) {
                 tempStack.push(move1.pop());
@@ -185,10 +184,10 @@ public class Solitaire {
     }
 
     private String moveMultipleCardStackToCardStack(int name1, int name2) {
-        Stack move1 = integerToCardStack(name1);
-        Stack move2 = integerToCardStack(name2);
-        if (!move1.peek().getColor().equals(move2.peek().getColor()))
-            if (move1.peek().getFaceName() == move2.peek().getFaceName()-1) {
+        Stack move1 = cardStacks[name1-1];
+        Stack move2 = cardStacks[name2-1];
+        if (!move1.list.get(move1.getMinOpen()).getColor().equals(move2.peek().getColor())) {
+            if (move1.list.get(move1.getMinOpen()).getFaceName() == move2.peek().getFaceName() - 1) {
                 for (int i = move1.getMaxOpen(); i >= move1.getMinOpen(); i--) {
                     tempStack.push(move1.pop());
                     move1.setMaxOpen(move1.getMaxOpen() - 1);
@@ -199,77 +198,88 @@ public class Solitaire {
                 }
                 return "Move Completed.";
             }
+        }
         return "Invalid Move.";
     }
 
-    private Stack integerToCardStack(int name) {
-        if (name == 1) {
-            return cardStack1;
-        }
-        else if (name == 2) {
-            return cardStack2;
-        }
-        else if (name == 3) {
-            return cardStack3;
-        }
-        else if (name == 4) {
-            return cardStack4;
-        }
-        else if (name == 5) {
-            return cardStack5;
-        }
-        else if (name == 6) {
-            return cardStack6;
-        }
-        else {
-            return cardStack7;
+    private Stack stringToEndStack(String name) {
+        if (name.equals("H")) {
+            return cardStacks[0];
+        } else if (name.equals("S")) {
+            return cardStacks[1];
+        } else if (name.equals("D")) {
+            return cardStacks[2];
+        } else {
+            return cardStacks[3];
         }
     }
 
-    private Stack stringToEndStack(String name) {
-        if (name.equals("Hearts")) {
-            return endStack1;
-        } else if (name.equals("Spade")) {
-            return endStack1;
-        } else if (name.equals("Diamonds")) {
-            return endStack1;
-        } else {
-            return endStack1;
+    private void displayGame() {
+        System.out.println("*************** GAME STATUS ***************\n");
+        System.out.println("** PACK **\t\t**** END STACKS ****");
+        System.out.println("\t\t\tEnd1\tEnd2\tEnd3\tEnd4");
+        System.out.print(pack.open().getFaceName() + pack.open().getSuit() + "\t\t\t");
+        for(int i = 0; i < 4; i++) {
+            System.out.print(endStacks[i].peek().getFaceName() + endStacks[i].peek().getSuit() + "\t\t");
+        }
+        System.out.println("\n******** CARD STACKS ********");
+        System.out.println("Stack1\tStack2\tStack3\tStack4\tStack5\tStack6\tStack7\t");
+
+        int maxSize = 0;
+        for (int i = 0; i < 7; i++){
+            if(cardStacks[i].getTop() > maxSize) {
+                maxSize = cardStacks[i].getTop();
+            }
+            //System.out.println(cardStacks[i].getTop());
+        }
+
+        for (int i = 0; i < maxSize+1; i++) {
+            for(int j = 0; j < 7; j++) {
+                if (cardStacks[j].getTop() >= i) {
+                    if(cardStacks[j].getMinOpen() <= i && i <= cardStacks[j].getMaxOpen()) {
+                        System.out.print(cardStacks[j].list.get(i).getFaceName() + cardStacks[j].list.get(i).getSuit() + "\t\t");
+                    }
+                    else{
+                        System.out.print("#\t\t");
+                    }
+                }
+                else {
+                    System.out.print("\t\t");
+                }
+            }
+            System.out.println();
         }
     }
 
     private Solitaire() {
-        Card[] deck;
-        cardStack1 = new Stack();
-        cardStack2 = new Stack();
-        cardStack3 = new Stack();
-        cardStack4 = new Stack();
-        cardStack5 = new Stack();
-        cardStack6 = new Stack();
-        cardStack7 = new Stack();
-        endStack1 = new Stack();
-        endStack2 = new Stack();
-        endStack3 = new Stack();
-        endStack4 = new Stack();
+        Card[] deck = new Card[52];
+
+        for(int i = 0; i < 7; i++) {
+            cardStacks[i] = new Stack();
+        }
+
+        for(int i = 0; i < 4; i++) {
+            endStacks[i] = new Stack();
+        }
+
         tempStack = new Stack();
         pack = new CircularList();
-        deck = new Card[52];
         int counter = 0;
 
         //creating the deck
         for (int i = 1; i < 14; i++){
-            deck[counter] = new Card(i, "Red", "Hearts");
+            deck[counter] = new Card(i, "Red", "H");
             counter++;
-            deck[counter] = new Card(i, "Black", "Spades");
+            deck[counter] = new Card(i, "Black", "S");
             counter++;
-            deck[counter] = new Card(i, "Red", "Diamonds");
+            deck[counter] = new Card(i, "Red", "D");
             counter++;
-            deck[counter] = new Card(i, "Black", "Clubs");
+            deck[counter] = new Card(i, "Black", "C");
             counter++;
         }
 
         //shuffling the cards
-        for (int i = 0; i < 60; i++){
+        for (int i = 0; i < 70; i++){
             int x = (int) (Math.random() * deck.length);
             Card temp = deck[0];
             deck[0] = deck[x];
@@ -280,51 +290,18 @@ public class Solitaire {
 
         //setting the cards
         for (int i = 0; i < 7; i++) {
-            if (i < 1) {
-                cardStack1.push(deck[counter]);
-                counter++;
+            for(int j = 0; j < 7; j++) {
+                if(i <= j) {
+                    cardStacks[j].push(deck[counter]);
+                    counter++;
+                }
             }
-            if (i < 2) {
-                cardStack2.push(deck[counter]);
-                counter++;
-            }
-            if (i < 3) {
-                cardStack3.push(deck[counter]);
-                counter++;
-            }
-            if (i < 4) {
-                cardStack4.push(deck[counter]);
-                counter++;
-            }
-            if (i < 5) {
-                cardStack5.push(deck[counter]);
-                counter++;
-            }
-            if (i < 6) {
-                cardStack6.push(deck[counter]);
-                counter++;
-            }
-
-            cardStack7.push(deck[counter]);
-            counter++;
-
         }
 
-        cardStack1.setMinOpen(cardStack1.getTop());
-        cardStack2.setMinOpen(cardStack2.getTop());
-        cardStack3.setMinOpen(cardStack3.getTop());
-        cardStack4.setMinOpen(cardStack4.getTop());
-        cardStack5.setMinOpen(cardStack5.getTop());
-        cardStack6.setMinOpen(cardStack6.getTop());
-        cardStack7.setMinOpen(cardStack7.getTop());
-
-        cardStack1.setMaxOpen(cardStack1.getTop());
-        cardStack2.setMaxOpen(cardStack2.getTop());
-        cardStack3.setMaxOpen(cardStack3.getTop());
-        cardStack4.setMaxOpen(cardStack4.getTop());
-        cardStack5.setMaxOpen(cardStack5.getTop());
-        cardStack6.setMaxOpen(cardStack6.getTop());
-        cardStack7.setMaxOpen(cardStack7.getTop());
+        for(int i = 0; i < 7; i++) {
+            cardStacks[i].setMinOpen(cardStacks[i].getTop());
+            cardStacks[i].setMaxOpen(cardStacks[i].getTop());
+        }
 
         //putting the remaining cards aside
         for (int i = counter; i < deck.length; i++) {
